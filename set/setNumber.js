f9e69cea373d860e9894f1fc55cc9f5cd4007320
HMooooo <it222219@trident.ac.jp>
2024-12-09 16:42:09 +0900

    set

--- set/setNumber.js
+++ set/setNumber.js
@@ -3,13 +3,7 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/11.0.1/firebase
 import { doc, setDoc } from "https://www.gstatic.com/firebasejs/11.0.1/firebase-firestore.js";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBFQUceYFlwmw_fMFkvls5MUEl24o10io0",
-    authDomain: "bingosystem-70a1f.firebaseapp.com",
-    projectId: "bingosystem-70a1f",
-    storageBucket: "bingosystem-70a1f.appspot.com",
-    messagingSenderId: "243294568856",
-    appId: "1:243294568856:web:987512ef130f816def2bed",
-    measurementId: "G-GSL9GQ1FQG"
+    "[API Config]"
 };
 
 //Firebaseの初期化
